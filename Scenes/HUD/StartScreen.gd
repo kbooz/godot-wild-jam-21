852fd3d76167965c67e6ef742016cb9632d6c02a
Inácio Schweller <iscmendonca@gmail.com>
2020-05-15 00:31:27 -0300

    Labels, clean up and more stuff

--- Scenes/HUD/StartScreen.gd
+++ Scenes/HUD/StartScreen.gd
@@ -5,10 +5,6 @@ onready var transitionAnimator = $CanvasLayer2/TransitionLayer/ColorRect/Animato
 
 func _ready():
 	Music.list_play()
-	
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://Scenes/World.tscn")
 
 func _on_TextureButton_pressed():
 	transitionLayer.layer = 2
