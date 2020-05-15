8078c2892d6ed544b62099364d3aebbbefa4014e
Inácio Schweller <iscmendonca@gmail.com>
2020-05-15 14:04:28 -0300

    Level Name system

--- Scenes/BaseLevel.gd
+++ Scenes/BaseLevel.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 onready var initialPosition = $InitialPosition
+onready var levelName = $Label
+
 var MainInstances = ResourceLoader.MainInstances
 var holes : Array = [];
 
 func _ready():
+	print(levelName.rect_size)
+	levelName.rect_position.x = (get_viewport().size.x / 4 - levelName.rect_size.x / 2)
 	MainInstances.Player.position = initialPosition.position
 	MainInstances.Player.initial_position = initialPosition.position
 	MainInstances.PlayerTrail.clear_points()
