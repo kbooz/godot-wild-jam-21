d7714fa8201684ed39fdf311bc9ca3291fc4d1b5
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2020-05-15 22:56:04 -0300

    Label animation entering

--- Scenes/BaseLevel.gd
+++ Scenes/BaseLevel.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 onready var initialPosition = $InitialPosition
+onready var labelAnimator = $LabelAnimator
+
+export(bool) var has_animation = false
 
 var MainInstances = ResourceLoader.MainInstances
 var holes : Array = [];
@@ -11,3 +14,6 @@ func _ready():
 	MainInstances.PlayerTrail.clear_points()
 	var tree = get_tree()
 	holes = tree.get_nodes_in_group("Holes")
+	if(has_animation):
+		labelAnimator.play("Entering")
+	
